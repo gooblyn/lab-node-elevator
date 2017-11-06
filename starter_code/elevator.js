8cd87111dd34ed49aa954b1354e8d849caafc8a9
Rocio Garcia <gooblyn@gmail.com>
2017-11-06 23:25:50 +0100

    Done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -2,6 +2,8 @@ class Elevator {
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
+    this.waitingList = [];
+    this.passengers = [];
     this.requests   = [];
     this.direction  = "up";
   }
@@ -15,23 +17,45 @@ class Elevator {
       if (this.requests.length === 0) {
         clearInterval(id);
       }
-    });
+    },4000);
   }
 
   update() {
     this.log();
   }
 
-  _passengersEnter() { }
+  _passengersEnter(person) {
+    this.passengers.push(person);
+    this.requests.push(person.destinationFloor);
+    console.log(`${person.name} has enter the elevator.`);
+  }
 
-  _passengersLeave() { }
+  _passengersLeave(person) {
+    console.log(`${person.name} has left the elevator.`);
+  }
 
   floorUp() {
-    if (this.floor < 10){
+    if (this.floor < this.MAXFLOOR){
       this.floor = this.floor+1;
     }
     else{
-      console.log("no vayas al cielo aun...");
+      console.log("You are on the top floor");
+      this.direction = "down";
+    }
+
+    let i;
+    for (i=0; i < this.waitingList.length; i++){
+      if(this.waitingList[i].originFloor == this.floor){
+        this._passengersEnter(this.waitingList[i]);
+        this.waitingList.splice(i,1);
+      }
+    }
+    let j;
+    for (j=0; j<this.passengers.length;j++){
+      if(this.passengers[j].destinationFloor == this.floor){
+        this._passengersLeave(this.passengers[j]);
+        this.passengers.splice(j,1);
+      }
     }
   }
 
@@ -40,12 +64,28 @@ class Elevator {
       this.floor = this.floor-1;
     }
     else{
-      console.log("no puedes ir al inframundo!");
+      console.log("You can't keep going down.");
+      this.direction = "up";
+    }
+    let i;
+    for (i=0; i < this.waitingList.length; i++){
+      if(this.waitingList[i].originFloor == this.floor){
+        this._passengersEnter(this.waitingList[i]);
+        this.waitingList.splice(i,1);
+      }
+    }
+    let j;
+    for (j=0; j<this.passengers.length;j++){
+      if(this.passengers[j].destinationFloor == this.floor){
+        this._passengersLeave(this.passengers[j]);
+        this.passengers.splice(j,1);
+      }
     }
   }
 
   call(person) {
-    this.requests.push(person);
+    this.requests.push(person.originFloor);
+    this.waitingList.push(person);
   }
 
   log() {
--- starter_code/index.js
+++ starter_code/index.js
@@ -7,17 +7,3 @@ let intervalId;
 intervalId = elevator.start();
 
 elevator.stop(intervalId);
-
-elevator.floorUp();
-elevator.log();
-
-elevator.floorDown();
-elevator.log();
-
-elevator.floorDown();
-elevator.log();
-
-let person = new Person("Rocío",0,4);
-elevator.call(person);
-
-console.log(elevator);
