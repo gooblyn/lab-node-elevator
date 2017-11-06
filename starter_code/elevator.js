329338de724a04502202a850b8b25492dce7b1c4
Rocio Garcia <gooblyn@gmail.com>
2017-11-06 18:04:50 +0100

    First iteration done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,30 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction  = "up";
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    return setInterval(() => {this.update();}, 1000);
+  }
+
+  stop(id) {
+    setTimeout(() => {clearInterval(id);},10000);
+  }
+
+  update() {
+    this.log();
+  }
+
   _passengersEnter() { }
+
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,9 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+let elevator = new Elevator();
+let intervalId;
+
+intervalId = elevator.start();
+
+elevator.stop(intervalId);
